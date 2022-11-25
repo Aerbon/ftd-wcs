7e23921ad8f9489a882d15843b1f1ec627b00f97
Aerbon <leo7904@gmail.com>
2022-11-25 16:50:40 +0000

    Lots of changes

--- script.lua
+++ script.lua
@@ -1,37 +1,62 @@
--- Aerbon Weapons Control Script.
+--- Aerbon Weapons Control Script.
 Options = { -- Options here
-    TrackingPeriod = 400 -- Amount of frames to keep data for.
+    TrackingPeriod = 400, -- Amount of frames to keep data for.
+    DiscardTimer = 80, -- Amount of frames a target has been missing for at which we forget it.
+    CoordinateMode = "World", -- What space to use for the coordinates of our target, one of "World", "Relative" or "Polar".
+    AveragingTime = "Auto" -- How far back into the past must we look, in order to determine the future? X frames or "Auto".
 }
 
 -- These are libraries that i think will come in handy.
-Buffer =
-{ -- An array sorta thing usefull for keeping track of data and stuff over time.
-    New = function(S) return {cur_pos = 0, size = S} end, --Returns an empty buffer.
-    Fill = function(B, V) for i = 0, B.size - 1, 1 do B[i] = V end end, --Fills the provided buffer B with V.
-    Push = function(B, V) --Pushes V into buffer B, replacing the oldest previous element.
-        B.cur_pos = (B.cur_pos + 1) % B.size
-        B[B.cur_pos] = V
+Buffer = { -- An array sorta thing usefull for keeping track of data and stuff over time.
+    Fill = function(self, V) for i = 0, self.size - 1, 1 do self[i] = V end end, --Fills buffer self with V.
+    Push = function(self, V) --Pushes V into buffer self, replacing the oldest previous element.
+        self.cur_pos = (self.cur_pos + 1) % self.size
+        self[self.cur_pos] = V
     end,
-    Get = function(B, I) --Returns element index I of buffer B, where I=0 is the latest element to be added.
-        I = Mathf.Clamp(I, 0, B.size - 1)
-        I = (B.cur_pos + B.size - I) % B.size
-        return B[I]
+    Get = function(self, I) --Returns element index I of buffer self, where I=0 is the latest element to be added.
+        I = Mathf.Clamp(I, 0, self.size - 1)
+        I = (self.cur_pos + self.size - I) % self.size
+        return self[I]
     end,
-    Set = function(B, I, V) --Replaces element index I of buffer B, where I=0 is the latest element to be added.
-        I = Mathf.Clamp(I, 0, B.size - 1)
-        I = (B.cur_pos + B.size - I) % B.size
-        B[I] = V
-    end
+    Set = function(self, I, V) --Replaces element index I of buffer self, where I=0 is the latest element to be added.
+        I = Mathf.Clamp(I, 0, self.size - 1)
+        I = (self.cur_pos + self.size - I) % self.size
+        self[I] = V
+    end,
+    cur_pos = 0,
+    size = 1
 }
 -- Gonna put a fourier transform library here. I figure it will help with predicting sinusoidal movements.
 
+-- Setup stuff.
+TargetInfo = { -- We will store our targeting data here, in TargetInfo[UID].
+    list = {}, -- Continuous list of the UIDs of all the targets we have stored, so we can iterate through them.
+    size = 0, -- Number of targets in the list.
+    Add = function(self, T)
+        self.list[self.size] = T.Id
+        self.size = self.size + 1
+        self[T.Id] = {
+            last_update = Time,
+            position = Buffer
+        }
+        self[T.Id].position.size = Options.AveragingTime
+        self[T.Id].position:Push(T.Position)
+
+    end
+}
+
 -- The almighty Update function!
 function Update(I)
-    --not gonna implement any of this until i know what i'm doing, but here's the gist of it.
+    Time = I:GetTime()
+    --not gonna implement most of this until i know what i'm doing, but here's the gist of it.
     --Get the current targets of our AI
-    --For each one, check if it is already in our target list.
-    --If yes, update it.
-    --If not, add it to the list.
+    for M = 0, I:GetNumberOfMainframes() - 1 do
+        for T = 0, I:GetNumberOfTargets(M) - 1 do
+            --For each one, check if it is already in our target list.
+            --If yes, update it.
+            --If not, add it to the list.
+        end
+    end
     --Check what weapons we have on the ship.
     --Calculate the future positions of each target for each weapon's time to hit.
     --Compare the predictions we made X frames ago to where the targets are now.
